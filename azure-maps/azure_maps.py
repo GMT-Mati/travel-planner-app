858819442af63dfc71b75e1d47ac6717a519bfab
Mateusz Gruszka <gruszka.mateusz87@gmail.com>
2023-09-04 20:12:45 +0200

    add models

--- azure-maps/azure_maps.py
+++ azure-maps/azure_maps.py
@@ -1,9 +1,27 @@
 class AzureMaps:
 	def __init__(self, api_key):
 		self.api_key = api_key
+		self.base_url = 'https://atlas.microsoft.com/'
 
 	def get_locations_info(self, location_name):
 		# Implement your Azure Maps integration code here
-		pass
+		# Implement Azure Maps API call to retrive location information
+		endpoint = f'{self.base_url}search/fuzzy/json'
+		params = {
+			'api-version' : '1.0',
+			'query' : ;ocation_name,
+			'subscription-key' : self.api_key
+		}
+
+		response = requests.get(endpoint, params=params)
+		if response.status_code == 200:
+			data = response.json()
+			# Parse and return location information
+			return data
+		else:
+			# Handle error gracefully
+			return None
+
+
 
 
