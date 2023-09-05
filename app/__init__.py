fd6d7df3af141ebb2675af0b567f2be998362501
Mateusz Gruszka <gruszka.mateusz87@gmail.com>
2023-09-05 15:40:45 +0200

    database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Load configuration from config.py
 app.config,from_object('config')
+
+# Initialize the database
+db = SQLAlchemy(app)
 
 from app import routes, models
