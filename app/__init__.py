4efafc9a02aa22960337917035bef22fc300c895
Mateusz Gruszka <gruszka.mateusz87@gmail.com>
2023-09-05 16:08:58 +0200

    log info

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging.handlers import RotatingFileHandler
 
-app = Flask(__name__)
+# Configure logging
+if not app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/travel_planner.log', maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
 
-# Load configuration from config.py
-app.config,from_object('config')
-
-# Initialize the database
-db = SQLAlchemy(app)
-
-from app import routes, models
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Travel Planner startup')
+ 
